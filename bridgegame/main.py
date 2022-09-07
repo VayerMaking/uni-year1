cce54a9e8f6b53a21ecf6c46a27c7df9997e2c90
Martin Vayer <vayer@Martins-MacBook-Air.local>
2022-09-07 13:59:23 +0200

    polish ui; mainly visual changes

--- bridgegame/main.py
+++ bridgegame/main.py
@@ -2,7 +2,6 @@ from random_word import RandomWords
 
 r = RandomWords()
 
-atempts = 0
 atemptsLeft = 10
 
 wordToGuess = r.get_random_word()
@@ -10,32 +9,51 @@ guessAtempts = []
 leftToGuess = list(wordToGuess)
 wordToDisplay = list("_" * len(wordToGuess))
 
-print(wordToGuess)
-
 while True:
-	print("word to guess: " + "".join(wordToDisplay))
-	print("previous guesses: {}".format(guessAtempts))
-	print(leftToGuess)
+
+	if atemptsLeft == 10:
+		print("\033c", end="")
+
+	print(
+		f"""
+		.______   .______       __   _______   _______  _______ 
+		|   _  \  |   _  \     |  | |       \ /  _____||   ____|
+		|  |_)  | |  |_)  |    |  | |  .--.  |  |  __  |  |__   
+		|   _  <  |      /     |  | |  |  |  |  | |_ | |   __|  
+		|  |_)  | |  |\  \----.|  | |  '--'  |  |__| | |  |____ 
+		|______/  | _| `._____||__| |_______/ \______| |_______|
+                                                        
+		"""
+	)
+
+	print("word to guess: " + " ".join(wordToDisplay))
+	print("---------------------------------------------------"+ "".join("-----" * len(guessAtempts)))
+	print("|-- previous guesses : {} --|--  atempts left: {} --|".format(guessAtempts, atemptsLeft))
+	print("---------------------------------------------------"+ "".join("-----" * len(guessAtempts)))
+
 	userInput = input("guess a letter: ")
 
 	if userInput in wordToGuess:
 		print("letter '{}' is the correct letter".format(userInput))
 		leftToGuess = list(filter((userInput).__ne__, leftToGuess))
-		# wordToDisplay.insert(wordToGuess.index(userInput), userInput)
-		wordToDisplay[wordToGuess.index(userInput)] = userInput
+
+		for i in range(len(wordToGuess)):
+			if wordToGuess[i] == userInput:
+				wordToDisplay[i] = userInput
 	else:
 		print("letter '{}' is not the correct letter".format(userInput))
+		atemptsLeft -= 1
 	
 	if atemptsLeft <= 0:
 		print("no more atempts left")
+		print("game over")
 		break
 
 	if len(leftToGuess) == 0:
-		print("you guessed the word!")
+		print("you guessed the word [ {} ]".format(wordToGuess))
 		break
 
 	guessAtempts.append(userInput)
-	atempts+=1
 
+	print("\033c", end="")
 
-print("it took '{}' atempts".format(atempts))
